dedeab42ffa061688590fc0e3b6aaeb9cc2ff2e7
Rob Thomson <rob@marotori.com>
2026-06-02 18:32:58 +0100

    Change exe name

--- src/make.cmd
+++ src/make.cmd
@@ -2,6 +2,8 @@
 setlocal
 cd /d %~dp0
 
+if not defined UPDATER_VERSION set UPDATER_VERSION=1.0.10
+
 echo [1/6] Checking for pyinstaller...
 pyinstaller --version >nul 2>&1
 if errorlevel 1 (
@@ -9,24 +11,24 @@ if errorlevel 1 (
     pip install pyinstaller || goto :error
 )
 
-echo [2/6] Generating version info...
+echo [2/6] Generating version info (%UPDATER_VERSION%)...
 python gen_version_info.py || goto :error
 
 echo [3/6] Compiling update_radio_gui.py to standalone EXE...
-python -m PyInstaller --onefile --noupx update_radio_gui.py --name update_radio_gui --windowed --version-file version_info.txt --icon icon.ico || goto :error
+python -m PyInstaller --onefile --noupx update_radio_gui.py --name rotorflight-lua-ethos-suite-updater --windowed --version-file version_info.txt --icon icon.ico || goto :error
 
-echo [4/6] Moving update_radio_gui.exe into parent folder...
-if exist ..\update_radio_gui.exe (
-    del ..\update_radio_gui.exe
+echo [4/6] Moving rotorflight-lua-ethos-suite-updater.exe into parent folder...
+if exist ..\rotorflight-lua-ethos-suite-updater.exe (
+    del ..\rotorflight-lua-ethos-suite-updater.exe
 )
-move /Y dist\update_radio_gui.exe ..\update_radio_gui.exe >nul
+move /Y dist\rotorflight-lua-ethos-suite-updater.exe ..\rotorflight-lua-ethos-suite-updater.exe >nul
 
 echo [5/6] Cleaning up build tree...
 rd /s /q build
 rd /s /q dist
-del /q update_radio_gui.spec
+del /q rotorflight-lua-ethos-suite-updater.spec
 
-echo [6/6] ✅ Build complete. update_radio_gui.exe is ready at: ..\update_radio_gui.exe
+echo [6/6] ✅ Build complete. rotorflight-lua-ethos-suite-updater.exe is ready at: ..\rotorflight-lua-ethos-suite-updater.exe
 goto :eof
 
 :error
