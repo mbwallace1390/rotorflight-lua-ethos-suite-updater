9f63fe2548d2cb7814a8bf41670fafd06d687322
Michael Wallace <mbwallace1390@gmail.com>
2026-07-03 23:42:27 -0500

    Use Michael's updater repository links

--- src/update_radio_gui_secure.py
+++ src/update_radio_gui_secure.py
@@ -5,12 +5,14 @@
 import updater_core as _core
 from updater_gui_safety import apply_gui_safety as _apply_gui_safety
 from updater_hardening import apply_hardening as _apply_hardening
+from updater_repository_identity import apply_repository_identity as _apply_repository_identity
 from updater_security import apply_verified_https as _apply_verified_https
 from updater_transaction_proxy_active import install_transaction_state as _install_transaction_state
 
 _apply_hardening(_core)
 _apply_verified_https(_core)
 _install_transaction_state(_core)
+_apply_repository_identity(_core)
 _apply_gui_safety(_core)
 
 for _name in dir(_core):
